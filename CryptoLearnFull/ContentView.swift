947588bfc90e08022cff756393d3a57098ffc916
VladMin10 <miniailowladislav@gmail.com>
2024-07-18 15:50:05 +0300

    Initial Commit

---
+++ CryptoLearnFull/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CryptoLearnFull
+//
+//  Created by Vladyslav Mi on 15.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+    faff
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoLearnFull/CryptoLearnFullApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoLearnFullApp.swift
+//  CryptoLearnFull
+//
+//  Created by Vladyslav Mi on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoLearnFullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
